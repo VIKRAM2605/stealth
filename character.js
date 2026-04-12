2fbddb3b9cd9e3c39d4d5dc5eabdd357549f42a6
VIKRAM2605 <vikrams.al24@gmail.com>
2026-04-12 20:11:18 +0530

    added a draw sprite and updating it functions

---
+++ character.js
@@ -0,0 +1,206 @@
+import { canvas, characterSpriteSheet, ctx, keys } from "./main.js";
+
+const characterSprite = {
+    up: {
+        frames: [
+            {
+                row: 2, col: 1
+            },
+            {
+                row: 2, col: 2
+            },
+            {
+                row: 2, col: 3
+            },
+            {
+                row: 2, col: 4
+            }
+        ]
+    },
+    left: {
+        frames: [
+            {
+                row: 3, col: 1
+            },
+            {
+                row: 3, col: 2
+            },
+            {
+                row: 3, col: 3
+            },
+            {
+                row: 3, col: 4
+            }
+        ]
+    },
+    right: {
+        frames: [
+            {
+                row: 4, col: 1
+            },
+            {
+                row: 4, col: 2
+            },
+            {
+                row: 4, col: 3
+            },
+            {
+                row: 4, col: 4
+            }
+        ]
+    },
+    down: {
+        frames: [
+            {
+                row: 1, col: 1
+            },
+            {
+                row: 1, col: 2
+            },
+            {
+                row: 1, col: 3
+            },
+            {
+                row: 1, col: 4
+            }
+        ]
+    },
+};
+
+const deathSprite = {
+    up: {
+        frames: [
+            {
+                row: 5, col: 4
+            },
+            {
+                row: 6, col: 1
+            },
+            {
+                row: 6, col: 2
+            }
+        ]
+    },
+    left: {
+        frames: [
+            {
+                row: 6, col: 3
+            },
+            {
+                row: 6, col: 4
+            },
+            {
+                row: 7, col: 1
+            }
+        ]
+    },
+    right: {
+        frames: [
+            {
+                row: 7, col: 2
+            },
+            {
+                row: 7, col: 3
+            },
+            {
+                row: 7, col: 4
+            }
+        ]
+    },
+    down: {
+        frames: [
+            {
+                row: 5, col: 1
+            },
+            {
+                row: 5, col: 2
+            },
+            {
+                row: 5, col: 3
+            }
+        ]
+    }
+};
+
+let isDead = false;
+let lastTime = 0;
+let player = {
+    x: 0,
+    y: 0,
+    currentFrame: "down",
+    frameIndex: 0,
+    frameTimer: 0,
+    speed: 100,
+}
+
+//update character only takes horizontal first.
+function updateCharacter(delta) {
+    let dx = 0, dy = 0;
+    if (keys.left) {
+        dx = -1;
+        player.currentFrame = "left";
+    }
+    if (keys.right) {
+        dx = 1;
+        player.currentFrame = "right";
+    }
+    if (keys.up) {
+        dy = -1;
+        player.currentFrame = "up";
+    }
+    if (keys.down) {
+        dy = 1;
+        player.currentFrame = "down";
+    }
+
+    if(dx && dy){
+        
+    }
+
+    player.x += dx * delta * player.speed;
+    player.y += dy * delta * player.speed;
+
+    if (dx !== 0 || dy !== 0) {
+        player.frameTimer += delta;
+        if (player.frameTimer >= 0.15) {
+            player.frameTimer = 0;
+            const frames = isDead ? deathSprite[player.currentFrame].frames : characterSprite[player.currentFrame].frames;
+            player.frameIndex = (player.frameIndex + 1) % frames.length;
+        }
+    } else {
+        player.frameIndex = 0;
+    }
+
+}
+
+function drawCharacter() {
+
+    let frames;
+    if (isDead) {
+        frames = deathSprite[player.currentFrame].frames;
+    } else {
+        frames = characterSprite[player.currentFrame].frames;
+    }
+
+    let frame = frames[player.frameIndex]
+
+    ctx.drawImage(
+        characterSpriteSheet,
+        (frame.col - 1) * 16, (frame.row - 1) * 16, 16, 16,
+        player.x, player.y, 16 * 2, 16 * 2
+    );
+};
+
+export function gameLoop(currentTime) {
+    let delta = (currentTime - lastTime) / 1000;
+    lastTime = currentTime;
+    if (delta > 0.1) delta = 0.1;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    updateCharacter(delta);
+
+    drawCharacter();
+
+    requestAnimationFrame(gameLoop)
+}
---
+++ main.js
@@ -0,0 +1,92 @@
+import { gameLoop } from "./character.js";
+
+export const canvas = document.getElementById("game-canvas");
+export const ctx = canvas.getContext('2d');
+
+const scale = 1;
+const width = 512;
+const height = 512;
+
+function resizeCanvas() {
+    const dpr = window.devicePixelRatio || 1;
+    canvas.width = width * dpr;
+    canvas.height = height * dpr;
+
+    canvas.style.width = width + 'px';
+    canvas.style.height = height + 'px';
+
+    ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
+
+    ctx.imageSmoothingEnabled = false;
+}
+
+window.addEventListener('resize', resizeCanvas);
+
+resizeCanvas();
+
+export const characterSpriteSheet = new Image();
+characterSpriteSheet.src = "assets/inspector_spritesheet.png";
+
+let loadedCount = 0;
+const imageCount = 1;
+
+function onImageLoad() {
+    loadedCount++;
+    if (loadedCount === imageCount) {
+        console.log("started")
+        requestAnimationFrame(gameLoop);
+    }
+};
+
+export const keys = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+}
+
+document.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const key = e.key.toLowerCase();
+    switch (key) {
+        case 'w':
+            keys.up = true;
+            return;
+        case 's':
+            keys.down = true;
+            return;
+        case 'a':
+            keys.left = true;
+            return;
+        case 'd':
+            keys.right = true;
+            return;
+    }
+});
+
+document.addEventListener('keyup', (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    //console.log(keys);
+
+    const key = e.key.toLowerCase();
+    switch (key) {
+        case 'w':
+            keys.up = false;
+            return;
+        case 's':
+            keys.down = false;
+            return;
+        case 'a':
+            keys.left = false;
+            return;
+        case 'd':
+            keys.right = false;
+            return;
+    }
+})
+
+characterSpriteSheet.onload = onImageLoad;
+
+console.log(canvas, ctx);
